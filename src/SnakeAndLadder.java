2accc3887822f25bb28bb0551cc7633b0075dfbb
KartikeyaMaan <kartikeya.maan1997@gmail.com>
2023-02-23 12:08:50 +0530

    ADDED : loop till win

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -4,11 +4,16 @@ public class SnakeAndLadder {
     public final static int SNAKE=2;
     public static void main(String[] args) {
         int startPos=0;
-        int dice=(int)((Math.random()*10)%6+1);
-        int option=(int)((Math.random()*10)%3);
-        switch(option){
-            case LADDER -> startPos+=dice;
-            case SNAKE -> startPos-=dice;
+        while(startPos<=100){
+            int dice=(int)((Math.random()*10)%6+1);
+            int option=(int)((Math.random()*10)%3);
+            switch(option){
+                case LADDER -> startPos+=dice;
+                case SNAKE -> startPos-=dice;
+            }
+            System.out.println("Position = "+startPos);
+            if(startPos<0)
+                startPos=0;
         }
     }
 }
