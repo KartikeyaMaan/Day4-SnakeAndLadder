d4261a372ce6229c4bc55be3cfe020466d7adc7d
KartikeyaMaan <kartikeya.maan1997@gmail.com>
2023-02-23 13:06:25 +0530

    REFACTORED : added play game function, refactored while loop in main

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -2,25 +2,44 @@ public class SnakeAndLadder {
 
     public final static int LADDER = 1;
     public final static int SNAKE = 2;
+    public static int rollCount = 0;
 
-    public static void main(String[] args) {
-        int startPos = 0;
-        int rollCount = 0;
-        while (startPos != 100) {
+    public static int playGame(int startPos) {
+        boolean repeat = true;
+        while (repeat) {
             rollCount++;
             int dice = (int) ((Math.random() * 10) % 6 + 1);
             int option = (int) ((Math.random() * 10) % 3);
             switch (option) {
                 case LADDER -> startPos += dice;
-                case SNAKE -> startPos -= dice;
+                case SNAKE -> {
+                    startPos -= dice;
+                    repeat = false;
+                }
+                default -> repeat =false;
             }
-            System.out.println("Position after " + rollCount + " roll = " + startPos);
             if (startPos < 0)
                 startPos = 0;
             if (startPos > 100)
                 startPos -= dice;
         }
-        System.out.println("Final Position = " + startPos);
+        return startPos;
+    }
+
+    public static void main(String[] args) {
+        int startPos1 = 0;
+        int startPos2 = 0;
+        while (startPos1 != 100 && startPos2 != 100) {
+            startPos1 = playGame(startPos1);
+            if(startPos1!=100)
+                startPos2 = playGame(startPos2);
+            System.out.println("Player 1 position = " + startPos1);
+            System.out.println("Player 2 position = " + startPos2);
+        }
+        if (startPos1 == 100)
+            System.out.println("Player 1 wins");
+        else
+            System.out.println("Player 2 wins");
         System.out.println("No of times dice rolled = " + rollCount);
     }
 }
