ad312e0b1a7afdaf0879a381b7baf9125389217e
KartikeyaMaan <kartikeya.maan1997@gmail.com>
2023-02-23 12:17:00 +0530

    ADDED : condition for if pos greater than 100

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -4,7 +4,7 @@ public class SnakeAndLadder {
     public final static int SNAKE=2;
     public static void main(String[] args) {
         int startPos=0;
-        while(startPos<=100){
+        while(startPos!=100){
             int dice=(int)((Math.random()*10)%6+1);
             int option=(int)((Math.random()*10)%3);
             switch(option){
@@ -14,6 +14,9 @@ public class SnakeAndLadder {
             System.out.println("Position = "+startPos);
             if(startPos<0)
                 startPos=0;
+            if(startPos>100)
+                startPos-=dice;
         }
+        System.out.println("Final Position = "+startPos);
     }
 }
