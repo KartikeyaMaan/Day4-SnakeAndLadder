d1904de8ad52244e84f178033b67ea810a4daa91
KartikeyaMaan <kartikeya.maan1997@gmail.com>
2023-02-23 12:21:17 +0530

    ADDED : report of roll count and position

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,22 +1,26 @@
 public class SnakeAndLadder {
 
-    public final static int LADDER=1;
-    public final static int SNAKE=2;
+    public final static int LADDER = 1;
+    public final static int SNAKE = 2;
+
     public static void main(String[] args) {
-        int startPos=0;
-        while(startPos!=100){
-            int dice=(int)((Math.random()*10)%6+1);
-            int option=(int)((Math.random()*10)%3);
-            switch(option){
-                case LADDER -> startPos+=dice;
-                case SNAKE -> startPos-=dice;
+        int startPos = 0;
+        int rollCount = 0;
+        while (startPos != 100) {
+            rollCount++;
+            int dice = (int) ((Math.random() * 10) % 6 + 1);
+            int option = (int) ((Math.random() * 10) % 3);
+            switch (option) {
+                case LADDER -> startPos += dice;
+                case SNAKE -> startPos -= dice;
             }
-            System.out.println("Position = "+startPos);
-            if(startPos<0)
-                startPos=0;
-            if(startPos>100)
-                startPos-=dice;
+            System.out.println("Position after " + rollCount + " roll = " + startPos);
+            if (startPos < 0)
+                startPos = 0;
+            if (startPos > 100)
+                startPos -= dice;
         }
-        System.out.println("Final Position = "+startPos);
+        System.out.println("Final Position = " + startPos);
+        System.out.println("No of times dice rolled = " + rollCount);
     }
 }
